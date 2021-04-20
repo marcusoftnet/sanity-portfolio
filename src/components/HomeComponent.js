dd83fbb8d9c8b679c7cc8dd314791cb6ec6a3bac
Marcus Hammarberg <marcusoft.net@gmail.com>
2021-04-20 14:31:06 +0200

    Home page styled

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,17 @@
+import image from '../cover.jpg';
+
 const HomeComponent = () => {
   return (
     <main>
-      <img src='' alt='' />
-      <section>
-        <h1>Hi, I'm Marcus!</h1>
+      <img
+        src={image}
+        alt='A sunset'
+        className='absolute object-cover w-full h-full'
+      />
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl text-green-600 font-bold cursive leading-none lg:leading-snug home-name'>
+          Hi, I'm Marcus!
+        </h1>
       </section>
     </main>
   );
